dbacfb6142ff0fbb46a282c24b48348cd5f64cbc
Kevin Luch <kevinluch@Kevins-MacBook-Pro.local>
2021-09-03 15:33:55 -0500

    First commit

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,54 @@
+from flask_app import app
+from flask import render_template, redirect, request, session, flash 
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
+
+from flask_app.models.user import User
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/users/register', methods=['POST'])
+def register_user():
+
+    data = {
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email'],
+        'password': request.form['password'],
+        'confirm_password': request.form['confirm_password'],
+    }
+
+    valid = User.validate_registration(data)
+
+    if valid:
+        User.create_user(data)
+        flash('Account created - log in son!')
+    return redirect('/')
+
+@app.route('/users/login', methods=['POST'])
+def login_user():
+
+    data = {
+        'email': request.form['email']
+    }
+
+    user = User.get_user_by_email(data)
+
+    if user == None:
+        flash('Email is invalid!')
+        return redirect('/')
+
+
+    if not bcrypt.check_password_hash(user.password, request.form['password']):
+        flash('Password is incorrect!')
+        return redirect('/')
+    
+    session['user_id'] = user.id 
+    session['user_email'] = user.email 
+
+    return redirect("/")
